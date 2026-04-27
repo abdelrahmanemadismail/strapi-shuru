cf7437462d3984857a3407338f5763df486130d7
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2026-04-28 00:25:30 +0300

    refactor: remove unnecessary pluginOptions for i18n localization in category schema

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -585,11 +585,6 @@ export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
   options: {
     draftAndPublish: false;
   };
-  pluginOptions: {
-    i18n: {
-      localized: true;
-    };
-  };
   attributes: {
     articles: Schema.Attribute.Relation<'manyToMany', 'api::article.article'>;
     children_categories: Schema.Attribute.Relation<
@@ -605,11 +600,12 @@ export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
           localized: true;
         };
       }>;
-    locale: Schema.Attribute.String;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
       'api::category.category'
-    >;
+    > &
+      Schema.Attribute.Private;
     name: Schema.Attribute.String &
       Schema.Attribute.Required &
       Schema.Attribute.Unique &
